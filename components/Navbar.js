34862002e4f7294545c6906d5a5b043bb1e9e5c1
iuliia sutygina <81769855+juliasut@users.noreply.github.com>
2021-09-28 23:57:47 -0600

    Create Navbar

---
+++ components/Navbar.js
@@ -0,0 +1,39 @@
+import Link from 'next/link';
+
+export default function Navbar() {
+  const { user, username} = { };
+
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>
+          <Link href="/">
+            <button>Feed</button>
+          </Link>
+        </li>
+
+        {username && (
+          <>
+          <li>
+            <Link href="/admin">
+              <button>Write Posts</button>
+            </Link>
+          </li>
+          <li>
+            <Link href={`/${username}`}>
+              <img src={user?.photoURL} />
+            </Link>
+          </li>
+          </>
+        )}
+        {!username && (
+          <li>
+            <Link href="/enter">
+              <button>Log in</button>
+            </Link>
+          </li>
+        )}
+      </ul>
+    </nav>
+  )
+}

39785ec012ea7796ffc05514db2ad5239f46bf08
iuliia sutygina <81769855+juliasut@users.noreply.github.com>
2021-09-29 22:51:31 -0600

    Set up google auth

--- pages/enter.js
+++ pages/enter.js
@@ -1,7 +1,43 @@
-export default function EnterPage({}) {
+import { auth, googleAuthProvider } from '../lib/firebase';
+
+export default function Enter(props) {
+  const user = null;
+  const username = null;
+
   return (
     <main>
-      <h1>Sign Up</h1>
+      {user ? (
+        !username ? (
+          <UsernameForm />
+        ) : (
+          <SignOutButton />
+        )
+      ) : (
+        <SignInButton />
+      )}
     </main>
-  )
+  );
+}
+
+// sign in with Google button
+function SignInButton() {
+  const signInWithGoogle = async () => {
+    await auth.signInWithPopup(googleAuthProvider);
+    // handle errors with try/catch block
+  };
+  return (
+    <button className="btn-google" onClick={signInWithGoogle}>
+      <img src={'/google.png'} /> Sign in with Google
+    </button>
+  );
+}
+
+// sign out button removes jwt stored in the browser to manage the authentication state
+function SignOutButton() {
+  return <button onClick={() => auth.signOut()}>Sign Out</button>;
+}
+
+// choose a custom username
+function UsernameForm() {
+  return null;
 }

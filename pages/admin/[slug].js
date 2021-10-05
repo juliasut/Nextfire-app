752fa783c90143ca8ee6095cbe1015ebb0237ff7
iuliia sutygina <81769855+juliasut@users.noreply.github.com>
2021-10-05 00:48:39 -0600

    SEO - add reusable metatags component

---
+++ pages/admin/[slug].js
@@ -0,0 +1,10 @@
+import Metatags from "../../components/Metatags";
+
+export default function AdminPostEdit({ }) {
+  return (
+    <main>
+      <Metatags title="admin page" />
+      <h1>Edit Post</h1>
+    </main>
+  )
+}

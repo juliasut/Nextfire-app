9f4a8dd7a6f1289d18d2103709ea2be832957a65
iuliia sutygina <81769855+juliasut@users.noreply.github.com>
2021-10-05 23:45:14 -0600

    Import Image uploader

--- pages/admin/[slug].js
+++ pages/admin/[slug].js
@@ -1,7 +1,7 @@
 import styles from '../../styles/Admin.module.css';
 import AuthCheck from '../../components/AuthCheck';
 import { firestore, auth, serverTimestamp } from '../../lib/firebase';
-
+import ImageUploader from '../../components/ImageUploader';
 import { useState } from 'react';
 import { useRouter } from 'next/router';
 
@@ -91,7 +91,7 @@ function PostForm({ defaultValues, postRef, preview }) {
       )}
 
       <div className={preview ? styles.hidden : styles.controls}>
-        
+      <ImageUploader />
       <textarea name="content" ref={register({
             maxLength: { value: 20000, message: 'content is too long' },
             minLength: { value: 10, message: 'content is too short' },

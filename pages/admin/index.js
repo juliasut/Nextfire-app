06d6724fdadb6a7535029187520778a064e6eb58
iuliia sutygina <81769855+juliasut@users.noreply.github.com>
2021-10-05 18:39:25 -0600

    Create an AuthCheck component to render content for signed-in users

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,9 +1,11 @@
-
+import AuthCheck from '../../components/AuthCheck';
 
 export default function AdminPostsPage({}) {
   return (
     <main>
-      
+      <AuthCheck>
+        
+      </AuthCheck>
     </main>
-  )
+  );
 }
